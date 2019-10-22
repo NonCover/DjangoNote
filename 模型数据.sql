69edcd02bbf6614e24eebaeeba7a23e4b6146ac3
NonCover <51185019+NonCover@users.noreply.github.com>
2019-10-22 23:53:13 +0800

    Create 模型数据.sql

---
+++ 模型数据.sql
@@ -0,0 +1,14 @@
+insert into myapp_grades(Cname, Cdate, Cgril, Cboy, isDelete) values
+	('c1', '2019-11-4', 8, 4, 0), 
+	('c2', '2019-10-3', 6, 5, 0), 
+	('c3', '2019-10-25', 7, 8, 0);
+
+insert into myapp_students(Sname, Sgender, Sage, isDelete, Sgrade_id) value
+	('小红', '女', 18, 0, 1),
+	('小南', '女', 19, 0, 1),
+	('小明', '男', 18, 0, 1),
+	('小强', '男', 21, 0, 1),
+	('小芳', '女', 16, 0, 1),
+	('猪', '无', 3, 1, 1);
+  
+#this is django model data
