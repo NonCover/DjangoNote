d43250a5325be7c92e56e41691bd9b868a38d0c2
NonCover <51185019+NonCover@users.noreply.github.com>
2019-10-22 23:54:04 +0800

    Update 模型数据.sql

--- 模型数据.sql
+++ 模型数据.sql
@@ -3,7 +3,7 @@ insert into myapp_grades(Cname, Cdate, Cgril, Cboy, isDelete) values
 	('c2', '2019-10-3', 6, 5, 0), 
 	('c3', '2019-10-25', 7, 8, 0);
 
-insert into myapp_students(Sname, Sgender, Sage, isDelete, Sgrade_id) value
+insert into myapp_students(Sname, Sgender, Sage, isDelete, Sgrade_id) values
 	('小红', '女', 18, 0, 1),
 	('小南', '女', 19, 0, 1),
 	('小明', '男', 18, 0, 1),
